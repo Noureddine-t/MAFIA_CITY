ca5d55069f9ccd961eac267d1465e9a7efbbb57b
Noureddine-t <129674802+Noureddine-t@users.noreply.github.com>
2025-01-27 05:49:00 +0100

    Pause menu implemented

---
+++ pause/pause_menu.gd
@@ -0,0 +1,45 @@
+extends Control
+#@onready var optionsMenu = preload("res://options_menu.tscn")
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+
+func _on_resume_pressed():
+	resume()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
+
+# TODO
+#func _on_options_pressed():
+	#resume()
+	#get_tree().change_scene_to_file("res://menu/menu.tscn")
+
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+
+
+func _on_change_character_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://assets/scenes/choose_character.tscn")
