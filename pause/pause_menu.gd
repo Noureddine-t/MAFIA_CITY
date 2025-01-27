2f45f70a6414a6e18e3b0dcaec96c63d4ad0420c
Noureddine-t <129674802+Noureddine-t@users.noreply.github.com>
2025-01-27 20:13:22 +0100

    Sound added to menu buttons and BugFix: menu stays open but hidden

--- pause/pause_menu.gd
+++ pause/pause_menu.gd
@@ -3,18 +3,22 @@ extends Control
 @onready var resume_button = $PanelContainer/VBoxContainer/Resume
 @onready var restart_button = $PanelContainer/VBoxContainer/Restart
 @onready var change_Character_button = $PanelContainer/VBoxContainer/ChangeCharacter
-@onready var options_button = $PanelContainer/VBoxContainer/Options
+@onready var setting_button = $PanelContainer/VBoxContainer/Settings
 @onready var quit_button = $PanelContainer/VBoxContainer/Quit
 
 func _ready():
+	set_menu_active(false)
 	$AnimationPlayer.play("RESET")
-	resume_button.grab_focus()
+	
 	
 func resume():
+	set_menu_active(false)
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 
 func pause():
+	set_menu_active(true)
+	resume_button.grab_focus()
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 
@@ -28,20 +32,63 @@ func _process(delta):
 	testEsc()
 	
 func _on_resume_pressed():
+	click_sound()
 	resume()
 
 func _on_restart_pressed() -> void:
+	click_sound()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_change_character_pressed() -> void:
+	click_sound()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://assets/scenes/choose_character.tscn")
 	
 	# TODO
-#func _on_options_pressed():
+#func _on_settings_pressed():
+	#click_sound()
 	#resume()
 	#get_tree().change_scene_to_file("res://menu/menu.tscn")
 	
 func _on_quit_pressed():
+	click_sound()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
+
+
+func hover_sound():
+	var hover = $hover
+	hover.play()
+	
+func click_sound():
+	var click = $click
+	click.play()
+	
+	
+func _on_resume_mouse_entered() -> void:
+	hover_sound()
+
+
+func _on_restart_mouse_entered() -> void:
+	hover_sound()
+
+
+func _on_change_character_mouse_entered() -> void:
+	hover_sound()
+
+
+func _on_settings_mouse_entered() -> void:
+	hover_sound()
+
+
+func _on_quit_mouse_entered() -> void:
+	hover_sound()
+	
+func set_menu_active(active: bool):
+	$PanelContainer.visible = active
+	for button in $PanelContainer/VBoxContainer.get_children():
+		if button is Button:
+			button.disabled = not active
