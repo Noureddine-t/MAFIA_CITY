414fa3ea1af41fb2de7ab6549a114ef5e98921bf
Noureddine-t <129674802+Noureddine-t@users.noreply.github.com>
2025-01-28 08:40:13 +0100

    Refactor: reorganize project files

---
+++ assets/scenes/ui/first_page.gd
@@ -0,0 +1,12 @@
+extends Control
+
+const next_scene = "res://assets/scenes/ui/choose_character.tscn"
+
+func _input(event):
+	if event.is_pressed():
+		_load_character_select_scene()
+
+func _load_character_select_scene():
+	var result = get_tree().change_scene_to_file(next_scene)
+	if result != OK:
+		print("Erreur lors du chargement de la scène :", result)
